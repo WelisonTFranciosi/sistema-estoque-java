a1ac67e8f377ffa4f7876db738ad0ed34d73c96e
Welison Franciosi <welison578@gmail.com>
2026-05-03 17:21:19 -0300

    Atualizando fluxo de alteração de estoque

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -7,22 +7,22 @@ import java.util.ArrayList;
 import Entities.Products;
 
 public class Program {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
         Scanner sc = new Scanner(System.in);
-        List<Products> products = new ArrayList<>();        
-        
+        List<Products> products = new ArrayList<>();
+
         int menu;
 
-        do{
+        do {
             System.out.println("----Menu----");
             System.out.println("1-Cadastrar produto / estoque");
             System.out.println("2-Alterar preço do produto");
             System.out.println("3-Alterar estoque do produto");
             System.out.println("4-Exibir produtos");
             System.out.println("5-Sair");
-            
+
             menu = sc.nextInt();
 
             switch (menu) {
@@ -32,35 +32,35 @@ public class Program {
                     System.out.println("Quantos produtos deseja cadastrar? ");
                     quantity = sc.nextInt();
 
-                    for (int i = 0; i< quantity; i++){
+                    for (int i = 0; i < quantity; i++) {
                         System.out.println("Cadastrar Produto/estoque");
                         System.out.println("Id: ");
                         int id = sc.nextInt();
-                            while (id <= 0) {
-                                System.out.println("Id deve ser maior que 0");
-                                System.out.println("Id: ");
-                                id = sc.nextInt();
-                            }
+                        while (id <= 0) {
+                            System.out.println("Id deve ser maior que 0");
+                            System.out.println("Id: ");
+                            id = sc.nextInt();
+                        }
                         System.out.println("Nome: ");
                         sc.nextLine();
                         String name = sc.nextLine();
                         System.out.println("Preço: ");
                         double price = sc.nextDouble();
-                            while (price < 0) {
-                                System.out.println("Preço não pode ser negativo ");
-                                System.out.println("Preço: ");
-                                price = sc.nextDouble();
-                            }
+                        while (price < 0) {
+                            System.out.println("Preço não pode ser negativo ");
+                            System.out.println("Preço: ");
+                            price = sc.nextDouble();
+                        }
                         System.out.println("Estoque: ");
                         int stock = sc.nextInt();
-                            while (stock < 0) {
-                                System.out.println("O estoque não pode ser negativo ");
-                                System.out.println("Estoque: ");
-                                stock = sc.nextInt();
-                            }
+                        while (stock < 0) {
+                            System.out.println("O estoque não pode ser negativo ");
+                            System.out.println("Estoque: ");
+                            stock = sc.nextInt();
+                        }
                         Products product = new Products(id, name, price, stock);
                         products.add(product);
-                    
+
                     }
                     System.out.println("Produtos cadastrados com sucesso!");
                     break;
@@ -70,47 +70,57 @@ public class Program {
                     System.out.println("Informe o ID do produto que deseja alterar ");
                     int id = sc.nextInt();
                     Products product = findProductById(products, id);
-                    if ( product != null) {
+                    if (product != null) {
                         System.out.println("Informe o novo proço do produto:");
                         product.changePrice(sc.nextDouble());
-                            while (product.getPrice() < 0) {
-                                System.out.println("Preço não pode ser negativo ");
-                                System.out.println("preço atual do produto: " + product.getPrice());
-                                System.out.println("Informe um valor até ficar positivo:");
-                                product.changePrice(sc.nextDouble());
-                                System.out.println("Preço do produto alterado com sucesso!");
-                            }
-                        System.out.println("Preço do produto alterado com sucesso!");
-                    }
-                        else {
-                            System.out.println("Produto com ID " + id + " não encontrado.");
+                        while (product.getPrice() < 0) {
+                            System.out.println("Preço não pode ser negativo ");
+                            System.out.println("preço atual do produto: " + product.getPrice());
+                            System.out.println("Informe um valor até ficar positivo:");
+                            product.changePrice(sc.nextDouble());
+                            System.out.println("Preço do produto alterado com sucesso!");
                         }
+                        System.out.println("Preço do produto alterado com sucesso!");
+                    } else {
+                        System.out.println("Produto com ID " + id + " não encontrado.");
+                    }
                     break;
-                    
+
                 case 3:
                     // Alterar estoque do produto
                     System.out.println("Informe o ID do produto: ");
                     int idstock = sc.nextInt();
                     Products productsStock = findProductStockById(products, idstock);
-                    if ( productsStock != null) {
-                        System.out.println("Informe a quantidade a ser removida ou adicionada: ");
+                    if (productsStock != null) {
+                        System.out.println("Digite um valor positivo para entrada ou negativo para saída: ");
                         int stockChange = sc.nextInt();
-                        if (stockChange > productsStock.getStock()) {
-                                while (productsStock.getStock() <0) {
-                                    System.out.println("Estoque do produto não pode ser negativo ");
-                                    System.out.println("Estoque atual do produto: " + productsStock.getStock());
-                                    System.out.println("informe a quantidade a ser removida ou adicionar ");
-                                    stockChange = sc.nextInt();
-                                    productsStock.descrementStock(- stockChange);
-                                    System.out.println("Estoque do produto alterado com sucesso!");
-                                }}
-                                else {
-                                    productsStock.incrementStock(stockChange);
-                                    System.out.println("Estoque do produto alterado com sucesso!");
-                                }}
+
+                        if (stockChange < 0) {
+                            int removeQuantity = -stockChange;
+
+                            if (removeQuantity > productsStock.getStock()) {
+                                System.out.println("Não é possivel  deixar o estoque negativo");
+                                System.out.println("quantidade atual de estoque do produto: " + productsStock.getStock());
+                                System.out.println("Informe um valor até ficar positivo: ");
+                                stockChange = sc.nextInt();
+                            }
+
                             else {
-                                    System.out.println("Produto com ID " + idstock + " não encontrado.");
+                                productsStock.incrementStock(stockChange);
+                                System.out.println("Estoque do produto alterado com sucesso!");
+                                System.out.println("Estoque atual: " + productsStock.getStock());
+                            }
                         }
+                        else {
+                                productsStock.incrementStock(stockChange);
+                                System.out.println("Estoque do produto alterado com sucesso!");
+                                System.out.println("Estoque atual: " + productsStock.getStock());               
+                            }
+                    }        
+                    else {
+                            System.out.println("Produto com ID " + idstock + " não encontrado.");
+                        }
+                       
                     break;
                 case 4:
                     // Exibir produtos
@@ -124,39 +134,32 @@ public class Program {
                     break;
                 case 5:
                     System.out.println("Saindo do programa...");
-                    break;            
-            
+                    break;
+
                 default:
                     System.out.println("Opção incorreta, tenta novamente.");
             }
-            
-                
-            
-        }while (menu != 5); 
 
-         
+        } while (menu != 5);
 
-        
-            
         sc.close();
-
 
     }
 
     public static Products findProductById(List<Products> products, int id) {
         for (Products product : products) {
-            if (product.getId() == id ) {
+            if (product.getId() == id) {
                 return product;
-            } 
+            }
         }
         return null;
     };
 
     public static Products findProductStockById(List<Products> productsStock, int idstock) {
         for (Products product : productsStock) {
-            if (product.getId() == idstock ) {
+            if (product.getId() == idstock) {
                 return product;
-            } 
+            }
         }
         return null;
 
